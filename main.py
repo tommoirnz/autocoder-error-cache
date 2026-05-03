6f7757afba134ea190c4c23962f464230cae130f
Tom Moir <tomspeechnz@gmail.com>
2026-05-04 06:49:36 +1200

    Update

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 #Data only for AutoCoder10. This file is just a placeholder and does nothing
-#Put  the json from code_cache  in c:/code_cache directory
+#Put  the jsons from code_cache  in c:/code_cache directory
